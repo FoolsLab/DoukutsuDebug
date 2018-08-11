e5bdbc109c8e5c190b20a6c3a527623ddcd50248
foolslab <ryota@hotmail.co.jp>
2018-08-11 13:44:19 +0900

    v1.1

--- DoukutsuDebug/Form1.Designer.cs
+++ DoukutsuDebug/Form1.Designer.cs
@@ -28,37 +28,56 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.Screen = new System.Windows.Forms.PictureBox();
-			((System.ComponentModel.ISupportInitialize)(this.Screen)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// Screen
-			// 
-			this.Screen.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.Screen.Location = new System.Drawing.Point(0, 0);
-			this.Screen.Name = "Screen";
-			this.Screen.Size = new System.Drawing.Size(618, 424);
-			this.Screen.TabIndex = 0;
-			this.Screen.TabStop = false;
-			this.Screen.Paint += new System.Windows.Forms.PaintEventHandler(this.Screen_Paint);
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(618, 424);
-			this.Controls.Add(this.Screen);
-			this.Name = "Form1";
-			this.Text = "DoukutsuDebug v0.1";
-			this.Load += new System.EventHandler(this.Form1_Load);
-			((System.ComponentModel.ISupportInitialize)(this.Screen)).EndInit();
-			this.ResumeLayout(false);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Screen = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.Screen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Screen
+            // 
+            this.Screen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Screen.Location = new System.Drawing.Point(0, 0);
+            this.Screen.Name = "Screen";
+            this.Screen.Size = new System.Drawing.Size(1159, 548);
+            this.Screen.TabIndex = 0;
+            this.Screen.TabStop = false;
+            this.Screen.Paint += new System.Windows.Forms.PaintEventHandler(this.Screen_Paint);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(767, 400);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(318, 69);
+            this.trackBar1.SmallChange = 20;
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1159, 548);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.Screen);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DoukutsuDebug v1.1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Screen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.PictureBox Screen;
-	}
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
 }
 
--- DoukutsuDebug/Form1.cs
+++ DoukutsuDebug/Form1.cs
@@ -12,7 +12,7 @@ using System.Threading;
 
 namespace DoukutsuDebug
 {
-	[StructLayout(LayoutKind.Sequential)]
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
 	struct CSData
 	{
 		public int MyCharX;
@@ -20,73 +20,304 @@ namespace DoukutsuDebug
 		public int MyCharVelX;
 		public int MyCharVelY;
 		public int Frame;
-	};
+        public byte BoostInfo;
+        public int MyCharCollisionState;
+
+        public int ScriptNowLocation;
+        public int pScriptStr;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 16)]
+        public string ScriptStr;
+
+        public UInt32 holdrand;
+    };
 
 	public partial class Form1 : Form
 	{
 		int handle;
 		CSData dat;
 		Font f;
+        ManualResetEvent formLoadWait = new ManualResetEvent(false), finishWait = new ManualResetEvent(false);
+        bool finishedFlag = false, datLoaded = false;
 
-		System.Threading.Timer timer;
+        int frameDelay = 0;
 
-		[DllImport("dddll.dll")]
-		static extern int FindCaveStory();
+        [DllImport("dddll.dll")]
+		static extern UInt32 FindCaveStory();
 
-		[DllImport("dddll.dll")]
-		static extern void GetCSData(int handle, ref CSData dat);
+        [DllImport("dddll.dll")]
+        static extern int OpenCaveStory(UInt32 pid);
 
-		public Form1()
-		{
-			InitializeComponent();
+        [DllImport("dddll.dll")]
+        static extern int GetTls(int handle, UInt32 pid);
+
+        [DllImport("dddll.dll")]
+		static extern void GetCSData(int handle, out CSData dat, int pTls);
+
+        [DllImport("dddll.dll")]
+        static extern bool WaitFrame(int handle, out UInt32 pid, out UInt32 tid, out UInt32 opt);
+
+        [DllImport("dddll.dll")]
+        static extern void ContinueFrame(UInt32 pid, UInt32 tid);
+
+        [DllImport("dddll.dll")]
+        static extern void Detach(int handle, UInt32 pid);
+
+        public Form1()
+        {
+            var okFlag = 0;
+            var setupWait = new ManualResetEvent(false);
+
+            var frameSync = Task.Run(() =>
+            {
+                var pid = FindCaveStory();
+                handle = OpenCaveStory(pid);
+                if (handle == 0)
+                {
+                    okFlag = -1;
+                    setupWait.Set();
+                    return;
+                }
+                else
+                {
+                    okFlag = 1;
+                    setupWait.Set();
+                }
+                var ptls = GetTls(handle, pid);
+                formLoadWait.WaitOne();
+                while (!finishedFlag)
+                {
+                    UInt32 tid, opt;
+                    if(WaitFrame(handle, out pid, out tid, out opt))
+                    {
+                        GetCSData(handle, out dat, ptls);
+                        datLoaded = true;
+                        if (frameDelay > 0)
+                        {
+                            Thread.Sleep(frameDelay);
+                        }
+                        ContinueFrame(pid, tid);
+                        Screen.Invalidate();
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                Detach(handle, pid);
+                finishWait.Set();
+            });
+            setupWait.WaitOne();
+            if(okFlag == -1)
+            {
+                throw new Exception("洞窟物語のオープンに失敗");
+            }
+            InitializeComponent();
 		}
 
 		private void Form1_Load(object sender, EventArgs e)
-		{
-			handle = FindCaveStory();
-			if(handle == 0)
-			{
-				MessageBox.Show("洞窟物語が起動されていません");
-				Application.Exit();
-			}
+        {
+            dat = new CSData();
 
-			dat = new CSData();
-
-			f = new Font("MS Gothic", 16);
-
-			TimerCallback callback = state =>
-			{
-				Screen.Invalidate();
-			};
-
-			timer = new System.Threading.Timer(callback, null, 100, 10);
-		}
+            f = new Font("MS Gothic", 16);
+            formLoadWait.Set();
+        }
 
 		private void Screen_Paint(object sender, PaintEventArgs e)
 		{
-			GetCSData(handle, ref dat);
 			e.Graphics.Clear(Color.FromArgb(25, 33, 66));
-			e.Graphics.DrawString(String.Format("Pos X:{0, 10}", dat.MyCharX), f, Brushes.White, new Point(0, 0));
-			e.Graphics.DrawString(String.Format("Pos Y:{0, 10}", dat.MyCharY), f, Brushes.White, new Point(0, 30));
-			e.Graphics.DrawString(String.Format("Vel X:{0, 10}", dat.MyCharVelX), f, Brushes.White, new Point(0, 70));
-			e.Graphics.DrawString(String.Format("Vel Y:{0, 10}", dat.MyCharVelY), f, Brushes.White, new Point(0, 100));
+            if(!datLoaded)
+            {
+                return;
+            }
+            e.Graphics.DrawString(
+                String.Format(
+                    "Pos X:{0, 10}    =   {1}0x{2, 8:X}    =    0x2000*{3, 4} {4} 0x{5, 4:X}",
+                    +dat.MyCharX,
+                    (dat.MyCharX < 0) ? '-' : ' ',
+                    Math.Abs(dat.MyCharX),
+                    (dat.MyCharX + 0x1000) / 0x2000,
+                    (((dat.MyCharX + 0x1000) % 0x2000 - 0x1000) < 0) ? '-' : '+',
+                    Math.Abs((dat.MyCharX + 0x1000) % 0x2000 - 0x1000)),
+                f, Brushes.White, new Point(0, 0));
 
-			{
-				Point p = new Point(130, 250);
-				Size sz = new Size(160, 160);
-				
-				e.Graphics.DrawRectangle(
-					new Pen(Brushes.White),
-					new Rectangle(p.X - sz.Width / 2, p.Y - sz.Height / 2, sz.Width, sz.Height));
 
-				e.Graphics.DrawLine(
-					new Pen(Brushes.Red),
-					p,
-					new Point(p.X + dat.MyCharVelX * sz.Width / 2 / 1536, p.Y + dat.MyCharVelY * sz.Height / 2 / 1536));
+            e.Graphics.DrawString(
+                String.Format(
+                    "Pos Y:{0, 10}    =   {1}0x{2, 8:X}    =    0x2000*{3, 4} {4} 0x{5, 4:X}",
+                    +dat.MyCharY,
+                    (dat.MyCharY < 0) ? '-' : ' ',
+                    Math.Abs(dat.MyCharY),
+                    (dat.MyCharY + 0x1000) / 0x2000,
+                    (((dat.MyCharY + 0x1000) % 0x2000 - 0x1000) < 0) ? '-' : '+',
+                    Math.Abs((dat.MyCharY + 0x1000) % 0x2000 - 0x1000)),
+                f, Brushes.White, new Point(0, 30));
 
-			}
 
-			e.Graphics.DrawString(String.Format("Frame:{0, 10}", dat.Frame), f, Brushes.White, new Point(0, 360));
-		}
-	}
+            e.Graphics.DrawString(
+                String.Format(
+                    "Vel X:{0, 10}    =   {1}0x{2, 8:X}",
+                    dat.MyCharVelX,
+                    (dat.MyCharVelX < 0) ? '-' : ' ',
+                    Math.Abs(dat.MyCharVelX)),
+                f, Brushes.White, new Point(0, 70));
+
+            e.Graphics.DrawString(
+                String.Format(
+                    "Vel Y:{0, 10}    =   {1}0x{2, 8:X}",
+                    dat.MyCharVelY,
+                    (dat.MyCharVelY < 0) ? '-' : ' ',
+                    Math.Abs(dat.MyCharVelY)),
+                f, Brushes.White, new Point(0, 100));
+
+            {
+                Point p = new Point(130, 250);
+                Size sz = new Size(160, 160);
+                
+                e.Graphics.DrawRectangle(
+                    new Pen(Brushes.White, 2),
+                    new Rectangle(
+                        p.X - sz.Width / 2, p.Y - sz.Height / 2,
+                        sz.Width, sz.Height));
+
+                { 
+                    Pen ColPen = new Pen(Brushes.Green, 3);
+                    Pen FloPen = new Pen(Brushes.Pink, 6);
+                    {
+                        if ((dat.MyCharCollisionState & 0x1000) > 0)
+                        {
+                            e.Graphics.DrawLine(
+                                FloPen,
+                                new Point(p.X - sz.Width / 2 - 10, p.Y - sz.Height / 2),
+                                new Point(p.X - sz.Width / 2 - 10, p.Y + sz.Height / 2));
+                        }
+                        if ((dat.MyCharCollisionState & 0x2000) > 0)
+                        {
+                            e.Graphics.DrawLine(
+                                FloPen,
+                                new Point(p.X - sz.Width / 2, p.Y - sz.Height / 2 - 10),
+                                new Point(p.X + sz.Width / 2, p.Y - sz.Height / 2 - 10));
+                        }
+                        if ((dat.MyCharCollisionState & 0x4000) > 0)
+                        {
+                            e.Graphics.DrawLine(
+                                FloPen,
+                                new Point(p.X + sz.Width / 2 + 10, p.Y - sz.Height / 2),
+                                new Point(p.X + sz.Width / 2 + 10, p.Y + sz.Height / 2));
+                        }
+                        if ((dat.MyCharCollisionState & 0x8000) > 0)
+                        {
+                            e.Graphics.DrawLine(
+                                FloPen,
+                                new Point(p.X - sz.Width / 2, p.Y + sz.Height / 2 + 10),
+                                new Point(p.X + sz.Width / 2, p.Y + sz.Height / 2 + 10));
+                        }
+                    }
+                    {
+                        if ((dat.MyCharCollisionState & 1) > 0)
+                        {
+                            e.Graphics.DrawLine(
+                                ColPen,
+                                new Point(p.X - sz.Width / 2, p.Y - sz.Height / 2),
+                                new Point(p.X - sz.Width / 2, p.Y + sz.Height / 2));
+                        }
+                        if ((dat.MyCharCollisionState & 2) > 0)
+                        {
+                            e.Graphics.DrawLine(
+                                ColPen,
+                                new Point(p.X - sz.Width / 2, p.Y - sz.Height / 2),
+                                new Point(p.X + sz.Width / 2, p.Y - sz.Height / 2));
+                        }
+                        if ((dat.MyCharCollisionState & 4) > 0)
+                        {
+                            e.Graphics.DrawLine(
+                                ColPen,
+                                new Point(p.X + sz.Width / 2, p.Y - sz.Height / 2),
+                                new Point(p.X + sz.Width / 2, p.Y + sz.Height / 2));
+                        }
+                        if ((dat.MyCharCollisionState & 8) > 0)
+                        {
+                            e.Graphics.DrawLine(
+                                ColPen,
+                                new Point(p.X - sz.Width / 2, p.Y + sz.Height / 2),
+                                new Point(p.X + sz.Width / 2, p.Y + sz.Height / 2));
+                        }
+                    }
+                    {
+                        if ((dat.MyCharCollisionState & 0x10) > 0)
+                        {
+                            e.Graphics.DrawLine(
+                                ColPen,
+                                new Point(p.X + sz.Width / 2, p.Y - sz.Height / 2),
+                                new Point(p.X - sz.Width / 2, p.Y + sz.Height / 2));
+                        }
+                        if ((dat.MyCharCollisionState & 0x20) > 0)
+                        {
+                            e.Graphics.DrawLine(
+                                ColPen,
+                                new Point(p.X - sz.Width / 2, p.Y - sz.Height / 2),
+                                new Point(p.X + sz.Width / 2, p.Y + sz.Height / 2));
+                        }
+                    }
+                    {
+                        if ((dat.MyCharCollisionState & 0x10000) > 0)
+                        {
+                            e.Graphics.DrawLine(
+                                ColPen,
+                                new Point(p.X - sz.Width / 2, p.Y - sz.Height / 2),
+                                new Point(p.X + sz.Width / 2, p.Y));
+                        }
+                        if ((dat.MyCharCollisionState & 0x20000) > 0)
+                        {
+                            e.Graphics.DrawLine(
+                                ColPen,
+                                new Point(p.X + sz.Width / 2, p.Y + sz.Height / 2),
+                                new Point(p.X - sz.Width / 2, p.Y));
+                        }
+                        if ((dat.MyCharCollisionState & 0x40000) > 0)
+                        {
+                            e.Graphics.DrawLine(
+                                ColPen,
+                                new Point(p.X - sz.Width / 2, p.Y + sz.Height / 2),
+                                new Point(p.X + sz.Width / 2, p.Y));
+                        }
+                        if ((dat.MyCharCollisionState & 0x80000) > 0)
+                        {
+                            e.Graphics.DrawLine(
+                                ColPen,
+                                new Point(p.X + sz.Width / 2, p.Y - sz.Height / 2),
+                                new Point(p.X - sz.Width / 2, p.Y));
+                        }
+                    }
+                }
+
+                {
+
+                    e.Graphics.DrawLine(
+                        new Pen(Brushes.Red, 4),
+                        p,
+                        new Point(
+                            p.X + dat.MyCharVelX * sz.Width / 2 / 1536,
+                            p.Y + dat.MyCharVelY * sz.Height / 2 / 1536));
+                }
+            }
+
+            e.Graphics.DrawString(String.Format("Frame:{0, 10}", dat.Frame), f, Brushes.White, new Point(0, 360));
+            e.Graphics.DrawString(String.Format("BoostState:{0, 10}", dat.BoostInfo), f, Brushes.White, new Point(0, 400));
+            e.Graphics.DrawString(String.Format("Collision:0x{0, 8:X}", dat.MyCharCollisionState), f, Brushes.White, new Point(0, 440));
+            e.Graphics.DrawString(String.Format("holdrand:{0, 10}", dat.holdrand), f, Brushes.White, new Point(0, 480));
+
+            e.Graphics.DrawString(String.Format("Cmd:{0, 10}", dat.ScriptStr.Replace("\r", "").Replace("\n", " ")), f, Brushes.White, new Point(0, 520));
+        }
+
+        private void Form1_FormClosed(object sender, FormClosingEventArgs e)
+        {
+            finishedFlag = true;
+            finishWait.WaitOne();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            frameDelay = trackBar1.Value;
+        }
+    }
 }
